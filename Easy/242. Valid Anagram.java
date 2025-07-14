16e46bd6a0edc3d18c2c7b02ae20a09c6b77b08c
Meghana <yedlameghana01@gmail.com>
2025-07-14 20:28:15 +0530

    Create 242. Valid Anagram.java

---
+++ Easy/242. Valid Anagram.java
@@ -0,0 +1,20 @@
+/* 242. Valid Anagram
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+Example :
+Input: s = "anagram", t = "nagaram"
+Output: true */ 
+
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        int []freq = new int[26];
+        for(int i=0;i<s.length();i++)    freq[s.charAt(i) - 'a']++;
+        for(int i=0;i<t.length();i++)   freq[t.charAt(i) - 'a']--;
+        for(int i=0;i<26;i++){
+            if(freq[i] != 0)    return false;
+        }
+        return true;
+    }
+}
